d3fdab20928dca6df40bb53c770d41a1d6efbdf6
Javelin Senyu <1020629114@qq.com>
2024-05-21 14:59:56 +0800

    飞行手杖的升级功能实现

--- src/main/java/icu/senyu/fly_apple/FlyAppleMod.java
+++ src/main/java/icu/senyu/fly_apple/FlyAppleMod.java
@@ -1,31 +1,30 @@
 package icu.senyu.fly_apple;
 
-import icu.senyu.fly_apple.block.BlockRegister;
-import icu.senyu.fly_apple.effects.EffectRegister;
 import icu.senyu.fly_apple.item.FlyAppleTab;
 import icu.senyu.fly_apple.item.ItemRegister;
-import net.minecraft.world.item.CreativeModeTab;
+import icu.senyu.fly_apple.block.BlockRegister;
+import icu.senyu.fly_apple.effects.EffectRegister;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(FlyAppleMod.MOD_ID)
 public class FlyAppleMod {
 
     public static final String MOD_ID = "fly_apple";
-    public static final CreativeModeTab TAB = new FlyAppleTab();
+    public static final FlyAppleTab TAB = new FlyAppleTab();
     private static final Logger LOGGER = LogManager.getLogger();
 
     public FlyAppleMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ItemRegister.ITEMS.register(bus);
-        BlockRegister.BLOCKS.register(bus);
-        EffectRegister.EFFECTS.register(bus);
+        ItemRegister.ITEMS.register(modEventBus);
+        BlockRegister.BLOCKS.register(modEventBus);
+        EffectRegister.EFFECTS.register(modEventBus);
     }
 
 }
