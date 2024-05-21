1f47a9c3c314de97e24441283e1d8d61c9838698
Javelin Senyu <1020629114@qq.com>
2024-05-21 17:29:08 +0800

    合成表和掉落物已经写好，只剩贴图上线

--- src/main/java/icu/senyu/fly_apple/listerner/EventListener.java
+++ src/main/java/icu/senyu/fly_apple/listerner/EventListener.java
@@ -2,12 +2,23 @@ package icu.senyu.fly_apple.listerner;
 
 import icu.senyu.fly_apple.FlyAppleMod;
 import icu.senyu.fly_apple.effects.EffectRegister;
+import icu.senyu.fly_apple.item.ItemRegister;
 import icu.senyu.fly_apple.item.items.FlyInsuranceItem;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.FlyingMob;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ambient.Bat;
+import net.minecraft.world.entity.animal.FlyingAnimal;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.boss.wither.WitherBoss;
+import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -36,16 +47,47 @@ public class EventListener {
 
 
     @SubscribeEvent
-    public static void onLivingDeath(LivingDeathEvent event) {
-        if (event.getEntity() instanceof Player player) {
-            if (event.getSource() == DamageSource.FALL) {
-                boolean hasInsurance = FlyInsuranceItem.activateFlyInsurance(player);
-                if (hasInsurance) {
-                    player.setHealth(2f);
-                    event.setCanceled(true);
-                }
+    public static void onLivingDrops(LivingDropsEvent event) {
+        if(!(event.getEntity() instanceof LivingEntity entity))return;
+        Level world = entity.level;
+        int lootingLevel = event.getLootingLevel();
+        boolean isBoss = entity instanceof WitherBoss || entity instanceof EnderDragon;
+        ItemStack floatEssence = new ItemStack(ItemRegister.FLOAT_ESSENCE.get());
+
+        if (isFlyingMob(entity)) {
+            float chance = 0.6f + (0.1f * lootingLevel);
+            if (world.random.nextFloat() < chance) {
+                floatEssence.setCount(world.random.nextInt(1, 2 + lootingLevel));
+                entity.spawnAtLocation(floatEssence.copy());
             }
         }
+
+        if (isBoss) {
+            entity.spawnAtLocation(new ItemStack(ItemRegister.MYTHICAL_FLOAT_ESSENCE.get()));
+            floatEssence.setCount(world.random.nextInt(6 + (lootingLevel * 3), 16 + (lootingLevel * 6)));
+            entity.spawnAtLocation(floatEssence.copy());
+        }
+    }
+
+    public static boolean isFlyingMob(LivingEntity entity) {
+        return entity instanceof FlyingAnimal || //·ÉÐÐ¶¯Îï
+                entity instanceof FlyingMob ||
+                entity instanceof Bat ||
+                entity instanceof Blaze;
+    }
+
+    @SubscribeEvent
+    public static void onLivingDeath(LivingDeathEvent event) {
+        if (!(event.getSource() == DamageSource.FALL)) return;
+        if (!(event.getEntity() instanceof Player player)) return;
+
+        boolean hasInsurance = FlyInsuranceItem.activateFlyInsurance(player);
+        if (hasInsurance) {
+            player.setHealth(2f);
+            event.setCanceled(true);
+        }
+
+
     }
 
 }
